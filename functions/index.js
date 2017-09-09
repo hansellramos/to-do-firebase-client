42e28abe18e37e7be7c50508a021a862f9131c88
Hansell Ramos <hansell.x.ramos.-nd@disney.com>
2017-09-09 13:47:20 -0500

    Hidden previous functions Added function to populate default user data when created

--- functions/index.js
+++ functions/index.js
@@ -1,33 +1,116 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+
 admin.initializeApp(functions.config().firebase);
 
-exports.onUserCreated = functions.database.ref('/users/{userId}').onCreate(event => {
-  var timestamp = new Date().getTime();
-  return event.data.ref.update({created: timestamp, updated: timestamp, id: event.params.userId});
-});
-
-exports.onTaskCreated = functions.database.ref('/tasks/{taskId}').onCreate(event => {
-  var timestamp = new Date().getTime();
-  return event.data.ref.update({created: timestamp, updated: timestamp, id: event.params.taskId});
-});
-
-exports.onTaskUpdate = functions.database.ref('/tasks/{taskId}').onUpdate(event => {
-  var current = {
-      priority: event.data.current.child('priority').val()
-    , name: event.data.current.child('name').val()
-    , dueDate: event.data.current.child('name').val()
-  };
-  var previous = {
-    priority: event.data.previous.child('priority').val()
-    , name: event.data.previous.child('name').val()
-    , dueDate: event.data.previous.child('name').val()
-  }
-  if (JSON.stringify(previous)!==JSON.stringify(current)) {
+/*
+exports.DatabaseOnUserCreated = functions.database.ref('/users/{userId}')
+  .onCreate(event => {
     var timestamp = new Date().getTime();
-    return event.data.ref.update({updated: timestamp});
-  } else {
-    return;
-  }
-});
+    return event.data.ref.update({created: timestamp, modified: timestamp, id: event.params.userId});
+  });
 
+exports.DatabaseOnTaskCreated = functions.database.ref('/tasks/{taskId}')
+  .onCreate(event => {
+    var timestamp = new Date().getTime();
+    return event.data.ref.update({created: timestamp, modified: timestamp, id: event.params.taskId});
+  });
+
+exports.DatabaseOnChecklistCreated = functions.database.ref('/checklist/{checklistId}')
+  .onCreate(event => {
+    var timestamp = new Date().getTime();
+    return event.data.ref.update({created: timestamp, modified: timestamp, id: event.params.checklistId});
+  });
+*/
+
+/*
+exports.DatabaseOnTaskModified = functions.database.ref('/tasks/{taskId}')
+  .onUpdate(event => {
+    var current = {
+        priority: event.data.current.child('priority').val()
+      , name: event.data.current.child('name').val()
+      , dueDate: event.data.current.child('dueDate').val()
+    };
+    var previous = {
+      priority: event.data.previous.child('priority').val()
+      , name: event.data.previous.child('name').val()
+      , dueDate: event.data.previous.child('dueDate').val()
+    }
+    if (JSON.stringify(previous)!==JSON.stringify(current)) {
+      var timestamp = new Date().getTime();
+      return event.data.ref.update({
+        modified: timestamp
+        , created: event.data.previous.child('created').val()
+        , id: event.data.previous.child('id').val()
+      });
+    } else {
+      return;
+    }
+  });
+*/
+
+/*
+exports.DatabaseOnUserModified = functions.database.ref('/users/{userId}')
+  .onUpdate(event => {
+    var current = {
+      email: event.data.current.child('email').val()
+      , checklists: event.data.current.child('checklists').val()
+    };
+    var previous = {
+      email: event.data.previous.child('email').val()
+      , checklists: event.data.previous.child('checklists').val()
+    }
+    if (JSON.stringify(previous)!==JSON.stringify(current)) {
+      var timestamp = new Date().getTime();
+      return event.data.ref.update({
+        modified: timestamp
+        , created: event.data.previous.child('created').val()
+        , id: event.data.previous.child('id').val()
+      });
+    } else {
+      return;
+    }
+  });
+*/
+
+exports.AuthOnUserCreated = functions.auth.user()
+  .onCreate(event => {
+    const user = event.data;
+    const today = new Date();
+    const userData = {
+      created: (new Date().getTime())
+      , modified: (new Date().getTime())
+      , email: user.email
+    };
+    const defaultCheklist = {
+      created: (new Date().getTime())
+      , modified: (new Date().getTime())
+      , name: 'My Checklist'
+      , owner: user.uid
+    };
+    const defaultFirstTask = {
+      created: (new Date().getTime())
+      , modified: (new Date().getTime())
+      , owner: user.uid
+      , completed: false
+      , name: 'My first task'
+      , priority: 2
+      , dueDate: ""+(today.getYear()+1900)+"-"+(today.getMonth()+1)+"-"+today.getDate()
+    }
+    admin.database().ref('users/'+user.uid).set(userData).then(userResult => {
+      admin.database().ref('checklists/').push(defaultCheklist).then(checklistResult => {
+        userData.checklists = {
+          0: { id: checklistResult.key }
+        };
+        defaultFirstTask.checklist = checklistResult.key;
+        admin.database().ref('users/'+user.uid).set(userData);
+        admin.database().ref('tasks/').push(defaultFirstTask).then(taskResult => {
+          defaultCheklist.tasks = {
+            0: { id: taskResult.key }
+          };
+          admin.database().ref('checklists/'+checklistResult.key).set(defaultCheklist);
+        });
+      });
+    });
+    return;
+  });
